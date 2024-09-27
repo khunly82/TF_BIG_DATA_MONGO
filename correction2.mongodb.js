551af1cc9c5d14f37e6718f34df7e6daa8edd73b
Khun <khun.ly@bstorm.be>
2024-09-27 16:18:12 +0200

    test

---
+++ correction2.mongodb.js
@@ -0,0 +1,50 @@
+// récupérer les 10 premiers films triés par ordre décroissant
+// sur le score
+
+// récupérer les 10 suivants
+
+// récupérer le titre, le rating de tous les films 
+// qui ont un rating égal à 3
+
+// récupérer le titre, 
+// l'année de tous les films qui sont sortis 
+// dans les années 90 (de 1990 à 1999)
+
+// récupérer le titre 
+// tous les films d'horreur 
+// sortis après 2000
+
+// récupérer le titre des films dans lesquels tom hanks a joué 
+// triés par année
+
+// récupérer le titre, le nombre d'acteurs 
+// des films d'aventure sortis dans les années 2000 
+// triés par rating
+
+// récupérer le titre, les acteurs des films 
+// dont le nombre d'acteurs = 5
+
+// récupérer le titre, les acteurs des films 
+// dont le nombre d'acteurs > 5
+
+// récupérer le nombre de films d'horreur ou de comédie
+
+// récupérer le nombre films qui contiennent le mot "christmas"
+
+// récupérer le titre, 
+// les 5 premiers acteurs (triés par ordre croissant) 
+// des films sortis dans les années 90 
+// et dont le genre est "Horror" ou "Comedy"
+// trié par année
+
+// Bonus
+
+// récupérer le titre, la liste des acteurs 
+// si le nombre d'acteurs est plus petit que 3 
+// sinon afficher le nombre d'acteurs
+
+// récupérer à l'aide de l'opérateur $cond
+// le titre des films, l'année, le score 
+    // qui sont sortis apres 2000
+    // si le score est plus grand que 5
+    // qui sont sortis avant 2000 sinon
