70def91db8d5618dc1b4067f6c22a578ff83d49a
Khun <khun.ly@bstorm.be>
2024-10-30 09:39:08 +0100

    ex 3

---
+++ correction3.mongodb.js
@@ -0,0 +1,43 @@
+// aggrégation (ATTENTION même si certaines requètes pourraient être réalisées avec une extraction simple, utiliser "aggregate")
+// récupérer 20 titres de films aléatoires
+
+
+// récupérer les 20 premiers films triés par ordre
+// croissant sur leur titre
+
+
+// récupérer les 20 suivants
+
+
+// récupérer toutes les propriétés des films en ajoutant leur score sur 100 
+// dans lesquels Tom Hanks a joué, 
+// triés par ordre décroissant sur leur score
+
+
+// récupérer la moyenne des scores sur 100 dans lesquels Tom Hanks a joué
+
+
+// recupérer tous les titres de films groupés par genre
+
+
+// recupérer les 10 premiers titres de films groupés par genre
+
+
+// recupérer les 5 meilleures moyennes des scores des films groupés par acteurs
+
+
+// récupérer la moyenne des scores groupée par genre et par année
+
+
+
+// récupérer par acteurs le nombre nombre de films dans lesquels ils ont joué.
+
+
+// récupérer par acteurs l'ensemble des détails (genre, quantité) dans lesquels ils ont joué
+
+// format souhaité: 
+// [{ 
+//    _id: john Doe, 
+//    details : [ { genre: comedy, quantite: 2 }, { genre: Horror, quantite: 5 } , ... (autres détails) } ]
+//  }, ... (autres acteurs)]
+
